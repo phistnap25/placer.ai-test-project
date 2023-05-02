399b6d25f465f772d6aa5b992ed92adc57fed9db
Remigio Cardenas <cardenaslee13@gmail.com>
2023-05-01 21:38:32 -0400

    Move bear token to .env

--- src/app/utils/api.ts
+++ src/app/utils/api.ts
@@ -8,7 +8,7 @@ export async function api({url}: APIParams){
     try{
         const response = await fetch(finalUrl,{
             headers: {
-                'Authorization': 'Bearer eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJ1c2VyIjp7InVzZXJfZW1haWwiOiJwb3dlcmZ1bGhpc3RvcnkyNUBnbWFpbC5jb20iLCJhcGlfdG9rZW4iOiJvZ25JVFBzZmoyZXlqRy1EYXN1Yk1RcmNESk5mUnRnLXFiS1BNM19tUkZWc2szbElVbUtmOVhmRTZDOWVTbVRzQl9nIn0sImV4cCI6MTY4MzA0MTAzMn0.SC2DQRiXesII8jUcLZftXyivxtMmGJ6J7OdvPz-7gVA'
+                'Authorization': process.env.REACT_APP_TOKEN || ''
             },
         });
         const responseJSON = await response.json();
